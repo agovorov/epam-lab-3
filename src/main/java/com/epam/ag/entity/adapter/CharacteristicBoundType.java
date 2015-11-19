a04bb20dd82699500b42efe309f688eed3df7ca8
Andy Govorov <thegovorovs@gmail.com>
2015-11-19 15:22:18 +0600

    Adapter. test 2

---
+++ src/main/java/com/epam/ag/entity/adapter/CharacteristicBoundType.java
@@ -0,0 +1,26 @@
+package com.epam.ag.entity.adapter;
+
+/**
+ * @author Govorov Andrey
+ */
+public class CharacteristicBoundType {
+
+    private Object key;
+    private Object value;
+
+    public void setKey(Object key) {
+        this.key = key;
+    }
+
+    public Object getKey() {
+        return key;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+}
--- src/main/java/com/epam/ag/entity/adapter/CharacteristicMapAdapter.java
+++ src/main/java/com/epam/ag/entity/adapter/CharacteristicMapAdapter.java
@@ -10,30 +10,29 @@ import java.util.Map;
  * @author Govorov Andrey
  */
 //public class CharacteristicMapAdapter extends XmlAdapter<String, Property> {
-public class CharacteristicMapAdapter extends XmlAdapter<String[], Properties> {
-
+public class CharacteristicMapAdapter extends XmlAdapter<Property[], Properties> {
 
     @Override
-    public Properties unmarshal(String[] v) throws Exception {
+    public Properties unmarshal(Property[] v) throws Exception {
         return null;
     }
 
     /*
-        class MapAdapter extends XmlAdapter<MapElements[], Map<String, Boolean>>
-    {
-      public MapElements[] marshal(Map<String, Boolean> arg0) throws Exception
-      {
-        MapElements[] mapElements = new MapElements[arg0.size()];
+            class MapAdapter extends XmlAdapter<MapElements[], Map<String, Boolean>>
+        {
+          public MapElements[] marshal(Map<String, Boolean> arg0) throws Exception
+          {
+            MapElements[] mapElements = new MapElements[arg0.size()];
 
-        int i = 0;
-        for (Map.Entry<String, Boolean> entry : arg0.entrySet())
-          mapElements[i++] = new MapElements(entry.getKey(), entry.getValue());
+            int i = 0;
+            for (Map.Entry<String, Boolean> entry : arg0.entrySet())
+              mapElements[i++] = new MapElements(entry.getKey(), entry.getValue());
 
-        return mapElements;
-      }
-         */
+            return mapElements;
+          }
+             */
     @Override
-    public String[] marshal(Properties v) throws Exception {
+    public Property[] marshal(Properties v) throws Exception {
         Property[] propertyArray = new Property[10];
 
         int i = 0;
@@ -41,22 +40,14 @@ public class CharacteristicMapAdapter extends XmlAdapter<String[], Properties> {
         Iterator it = map.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry pair = (Map.Entry)it.next();
-            Property property = (Property) pair.getValue();
 
+            Property property = (Property) pair.getValue();
             propertyArray[i] = property;
+
             i++;
-            // System.out.println(pair.getKey() + " => " + property.getValue() );
+            System.out.println(pair.getKey() + " => " + property.getValue() );
             it.remove(); // avoids a ConcurrentModificationException
         }
-
-        // Test
-        String[] s = new String[6];
-        s[0] = new String("test 1");
-        s[1] = new String("test 2");
-        s[2] = new String("test 3");
-        s[3] = new String("");
-        s[4] = new String("");
-        s[5] = new String("");
-        return s;
+        return propertyArray;
     }
 }
